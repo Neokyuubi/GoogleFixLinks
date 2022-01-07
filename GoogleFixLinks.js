b2cae431df6d4829ffb9c56f2596711b328dba8c
Neokyuubi <40678329+Neokyuubi@users.noreply.github.com>
2022-01-07 02:26:07 +0100

    Update GoogleFixLinks.js

--- GoogleFixLinks.js
+++ GoogleFixLinks.js
@@ -1,17 +1,17 @@
 // ==UserScript==
 // @name         Fix Google Links
-// @version      0.6
+// @version      0.7
 // @description  Prevent google page from replacing search results links with the redirection ones
 // @author       Neokyuubi
-// @match        https://www.google.com/search?
+// @include      /^https?://www\.google\.com/search/
 // @icon         https://www.google.com/favicon.ico
 // @grant        none
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @run-at       document-idle
+// @namespace https://greasyfork.org/users/687731
 // ==/UserScript==
 
-(function () {
-  'use strict';
+(function(){
 
     $("#rso a[data-jsarwt]").each(function()
     {
